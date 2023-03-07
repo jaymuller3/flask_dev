40468efc8eab31c0c80640844c6a2acb0ed49b30
Jay <38884883+jaymuller3@users.noreply.github.com>
2023-03-07 15:56:08 -0500

    added jobitem.html, added css via bootstrap

--- start.py
+++ start.py
@@ -4,9 +4,22 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+jobs = [
+    {'id': 1,
+     'title': 'Data Scientist',
+     'location': 'San Francisco',
+     'salary': '$100,000'    
+    },
+    {'id': 2,
+     'title': 'Frontend Engineer',
+     'location': 'Remote',
+     'salary': '$120,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=jobs)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug = True)
